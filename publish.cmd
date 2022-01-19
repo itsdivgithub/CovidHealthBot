2cfbf902c438194fc16faeac54aacb02b0b5d73e
Divyanshu Singh <92361650+itsdivgithub@users.noreply.github.com>
2022-01-19 23:45:41 +0530

    init

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild QnABotAllFeatures.sln -p:DeployOnBuild=true -p:PublishProfile=.\covidhealthbot01-Web-Deploy.pubxml -p:Password=oZRetjP109QdEQsK0lCobzfE7gZbeEHQ3rmkEw3kx68cBsxPkBmy4DbnN1jz
+
